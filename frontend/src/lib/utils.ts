afkarxyz/SpotiFLAC

commit ae8b6104624cb1b84e357adcb39dfa55480693b1

Trim whitespace in sanitizePath function (#215)
Add whitespace trimming to sanitizePath function.

frontend/src/lib/utils.ts
@@ -9,6 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 
 
 export function sanitizePath(input: string, os: string): string {
+  let sanitized = input.trim(); // it will trim whitespace
   if (os === "Windows") {
     return input.replace(/[<>:"/\\|?*]/g, "_");
   }

commit 9cecafb44efbcfb19272a021db44a1a526fa3c6e

correctly implement whitespace trimming in sanitizePath
this fixes a regression introduced by #215

frontend/src/lib/utils.ts
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 
 
 export function sanitizePath(input: string, os: string): string {
-  let sanitized = input.trim(); // it will trim whitespace
+  input = input.trim(); // it will trim whitespace
   if (os === "Windows") {
     return input.replace(/[<>:"/\\|?*]/g, "_");
   }